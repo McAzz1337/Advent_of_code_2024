9e1b24190a31fd5edd26c4f256837e2d08e44210
MarcF <marcf91.mf@gmail.com>
2024-12-04 02:21:21 +0100

    Solved day3

---
+++ src/day3.rs
@@ -0,0 +1,122 @@
+use crate::{puzzle_result::PuzzleResult, util::file_io::get_input};
+
+use regex::Regex;
+
+pub fn day3() {
+    let input = get_input(3);
+    let mut result = PuzzleResult::<i32, i32>::new(3);
+    result.result_part_1(part1(&input));
+    result.result_part_2(part2(&input));
+    println!("{result}");
+}
+
+fn extract_numbers(s: &str) -> (i32, String) {
+    let regex = Regex::new(r"mul\(\d+,\d+\)").unwrap();
+    if let Some(reg) = regex.captures(s) {
+        let m = reg
+            .iter()
+            .filter_map(|cap| cap.map(|m| m.as_str()))
+            .collect::<Vec<&str>>()
+            .join(" ");
+        let start = m.find("(").unwrap() + 1;
+        let end = m.find(")").unwrap();
+        let m = &m[start..end];
+        let nums: Vec<i32> = m
+            .split(",")
+            .into_iter()
+            .map(|s| s.parse::<i32>().unwrap())
+            .collect();
+        let end = reg.get(0).map(|m| m.end()).unwrap();
+        (nums[0] * nums[1], s[end..].to_string())
+    } else {
+        (0, String::new())
+    }
+}
+
+fn process_line(s: &String) -> i32 {
+    let mut s = s.clone();
+    let mut acc = 0;
+    while !s.is_empty() {
+        let (num, new_str) = extract_numbers(&s);
+        acc += num;
+        s = new_str;
+    }
+    acc
+}
+
+fn part1(input: &Vec<String>) -> i32 {
+    input.iter().map(process_line).sum()
+}
+
+fn take_from_to(s: &String, start: &str, end: &str) -> (String, String) {
+    let start_reg = Regex::new(start).unwrap();
+    let end_reg = Regex::new(end).unwrap();
+    if let Some(reg) = start_reg.captures(s) {
+        let i = reg.get(0).map(|m| m.start()).unwrap();
+        if let Some(reg) = end_reg.captures(s) {
+            let j = reg.get(0).map(|m| m.start()).unwrap();
+            eprintln!("i {i} j {j}");
+            eprintln!("cap = {}", s[i..j].to_string());
+            (s[i..j].to_string(), s[j + 5..].to_string())
+        } else {
+            (s[i..].to_string(), String::new())
+        }
+    } else {
+        (String::new(), String::new())
+    }
+}
+
+fn get_enabled_sections(mut s: String) -> String {
+    s = String::from("do") + s.as_str();
+    let mut res = String::new();
+    while !s.is_empty() {
+        let (take, rest) = take_from_to(&s, "do", "don't");
+        res.push_str(take.as_str());
+        s = rest;
+    }
+    res
+}
+
+fn part2(input: &Vec<String>) -> i32 {
+    let input = input.iter().fold(String::new(), |a, b| a + b.as_str());
+    let input = get_enabled_sections(input);
+    process_line(&input)
+}
+
+#[cfg(test)]
+mod tests {
+
+    use regex::Regex;
+
+    use super::{part1, part2};
+    use crate::util::file_io::get_test_input;
+
+    #[test]
+    fn test_part1() {
+        let input = get_test_input(3);
+        let res = part1(&input);
+        assert_eq!(res, 161);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = get_test_input(3);
+        let res = part2(&input);
+        assert_eq!(res, 48);
+    }
+
+    #[test]
+    fn test_reg() {
+        let start = Regex::new("do").unwrap();
+        let end = Regex::new("don't").unwrap();
+        let input = get_test_input(3);
+        if let Some(s) = start.captures(input[0].as_str()) {
+            let i = s.get(0).map(|m| m.start()).unwrap();
+            assert_eq!(&input[0][i..i + 2], "do");
+        }
+        if let Some(s) = end.captures(input[0].as_str()) {
+            let i = s.get(0).map(|m| m.start()).unwrap();
+            assert_eq!(&input[0][i..i + 5], "don't");
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 pub mod day1;
 pub mod day2;
+pub mod day3;
 pub mod puzzle_result;
 pub mod util;
 
 use day1::day1;
 use day2::day2;
+use day3::day3;
 
 fn main() {
     day1();
     day2();
+    day3();
 }
